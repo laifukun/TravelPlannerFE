8b3ac37f4c88e8724b1c9ab065a4d49e415f9bc5
ZhengDog <xiezhengcn@gmail.com>
2021-06-24 15:55:05 -0700

    added a test POI on Map and infoWindow
    added a test POI on Map and infoWindow

--- src/SharedComponents/Map.js
+++ src/SharedComponents/Map.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {GoogleMap, useLoadScript, Marker, InfoWindow,} from "@react-google-maps/api";
 import * as POIData from "./POI-data-test.json";
+import mapStyles from "../styles/mapStyles";
 
 const libraries = ["places"];
 const mapContainerStyle = {
     width: '100vw',
     height: '87.2vh',
 };
+
+const imageStyle = {
+    float: 'left',
+    width: 100
+}
+
+// const textStyle = {
+//     textalign: 'left'
+// }
 const options = {
-    // styles: mapStyles,
+    styles: mapStyles,
     disableDefaultUI: true,
     zoomControl: true,
 };
@@ -18,6 +28,8 @@ const center = {
 };
 
 function Map() {
+    const[selectedPOI, setSelectedPOI] = useState(null);
+
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLEMAP_API,
         libraries,
@@ -49,13 +61,48 @@ function Map() {
             >
                 {POIData.features.map(POI => (
                     <Marker
-                        key = {POI.properties.PARK_ID}
+                        key = {POI.properties.poiid}
                         position = {{
                             lat: POI.geometry.coordinates[1],
                             lng: POI.geometry.coordinates[0]
                         }}
+                        onClick = {() => {
+                            setSelectedPOI(POI);
+                        }}
+                        // icon = {{
+                        //     url: " ",
+                        //     scaledSize: new window.google.maps.Size(25, 25)
+                        // }}
                     />
                 ))}
+
+                {selectedPOI && (
+                    <InfoWindow
+                        position = {{
+                            lat: selectedPOI.geometry.coordinates[1],
+                            lng: selectedPOI.geometry.coordinates[0]
+                        }}
+                        onCloseClick = {() => {
+                            setSelectedPOI(null);
+                        }}
+                    >
+                        <div>
+                            <div >
+                                <img 
+                                    src={selectedPOI.properties.imageUrl}
+                                    style = {imageStyle}
+                                />
+                            </div>
+                            <div >
+                                <b>{selectedPOI.properties.NAME}</b>
+                                <br/>
+                                Address: {selectedPOI.properties.ADDRESS}
+                                <br/>
+                                Description: {selectedPOI.properties.description}
+                            </div>
+                        </div>
+                    </InfoWindow>
+                )}
             </GoogleMap>
         </div>
     );
