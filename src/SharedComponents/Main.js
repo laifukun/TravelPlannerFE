1c62ff62a0a723b1dd7b8fd93e3a9d6e8b26f804
Chong Xu <cx44@duke.edu>
2021-06-24 21:09:04 +0800

    loginform完成，思考canvas和main

--- src/SharedComponents/Main.js
+++ src/SharedComponents/Main.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-import { Row, Col } from "antd";
-import Canvas from  './Canvas';
+import {Layout} from "antd";
+import Canvas from  "./Canvas";
 import POIInstruction from  './POIInstruction';
 import SearchResults from './SearchResults';
 import KeywordSearch from './KeywordSearch';
+import LoginForm from "../BeforeLogin/LoginForm";
+import { useState } from "react";
+
+
+const { Content } = Layout;
 
 function Main() {
+    const [authed, setAuthed] = useState(false);
+    
     return (
-      <Canvas />
-    );
+        <Layout style={{ height: "100vh" }}>
+            {authed ? (
+              <Content
+                    style={{
+                    padding: "50px",
+                    maxHeight: "calc(100% - 64px)",
+                    overflowY: "auto",
+                }}>  This is content!   </Content>
+            ) : (
+                <Content
+                    style={{
+                    padding: "50px",
+                    maxHeight: "calc(100% - 64px)",
+                    overflowY: "auto",
+                }}> 
+                    <LoginForm onSuccess={() => setAuthed(true)} />
+                    <Canvas />
+                </ Content>
+            )}
+        
+        </Layout>
+      );
   }
   
 export default Main;
