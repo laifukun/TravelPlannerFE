8bf195c5375cf0c63b4330d420fec008d7abd1d2
Chong Xu <cx44@duke.edu>
2021-06-24 09:54:01 +0800

    安装antd以及统一main

---
+++ src/SharedComponents/Main.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import Canvas from  './Canvas';
+import POIInstruction from  './POIInstruction';
+import SearchResults from './SearchResults';
+import KeywordSearch from './KeywordSearch';
