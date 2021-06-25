d364daa9129246897459c7293725f388679499a6
Kun Yu <yupuqun8@gmail.com>
2021-06-25 10:33:13 -0500

    route page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './styles/App.css';
 import Header from './SharedComponents/Header';
 import Footer from './SharedComponents/Footer';
 import Canvas from './SharedComponents/Canvas';
+import Route from './SharedComponents/Route';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Route />
      <Canvas />
      <Footer />
     </div>
