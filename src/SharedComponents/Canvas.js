f1193d563879cd36c2e6c16e121a637df546d74d
Fukun Lai <laifukun@gmail.com>
2021-06-24 22:18:40 -0500

    merge

--- src/SharedComponents/Canvas.js
+++ src/SharedComponents/Canvas.js
@@ -2,20 +2,12 @@
 import '../styles/Canvas.css';
 import KeywordSearch from './KeywordSearch';
 import Map from './Map';
-<<<<<<< HEAD
-
-=======
->>>>>>> 388db0f9d71349fa6c81a33b0724349dd94ad0dd
 
 function Canvas() {
 
     return (
         <div className="site-drawer-render-in-current-wrapper">
-<<<<<<< HEAD
             <KeywordSearch />            
-=======
-            <KeywordSearch />
->>>>>>> 388db0f9d71349fa6c81a33b0724349dd94ad0dd
             <Map />
             
         </div>
