be7cb4bb3682d85a09d2c75a8df286bfa5d4ed56
Zhaoru Shang <56742567+ZhaoruShang@users.noreply.github.com>
2021-06-25 20:47:54 -0700

    Add Route

--- src/SharedComponents/Map.js
+++ src/SharedComponents/Map.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
-import {GoogleMap, useLoadScript, Marker, InfoWindow,} from "@react-google-maps/api";
+import React, { useState, } from "react";
+import { GoogleMap, useLoadScript, Marker, InfoWindow, DirectionsService, DirectionsRenderer } from "@react-google-maps/api";
 import * as POIData from "./POI-data-test.json";
 import mapStyles from "../styles/mapStyles";
+import PropTypes from 'prop-types'
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -28,10 +29,16 @@ const center = {
     lng: -73.985664,
 };
 
-function Map() {
-    const[selectedPOI, setSelectedPOI] = useState(null);
+const DirectionsPropTypes = {
+    styles: PropTypes.shape({
+        container: PropTypes.object.isRequired,
+    }).isRequired,
+}
 
-    const {isLoaded, loadError} = useLoadScript({
+function Map( {styles} ) {
+    const [selectedPOI, setSelectedPOI] = useState(null);
+
+    const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLEMAP_API,
         libraries,
     });
@@ -41,72 +48,242 @@ function Map() {
         mapRef.current = map;
     }, [])
 
-    const panTo = React.useCallback(({lat, lng}) => {
-        mapRef.current.panTo({lat, lng});
+    const panTo = React.useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng });
         mapRef.current.setZoom(14);
     }, [])
-    
+
+    const [response, setResponse] = React.useState(null)
+    const [travelMode, setTravelMode] = React.useState('DRIVING')
+    const [origin, setOrigin] = React.useState('')
+    const [destination, setDestination] = React.useState('')
+    const originRef = React.useRef()
+    const destinationRef = React.useRef()
+
+    const directionsCallback = React.useCallback((res) => {
+        console.log(res)
+
+        if (res !== null) {
+            if (res.status === 'OK') {
+                setResponse(res)
+            } else {
+                console.log('response: ', res)
+            }
+        }
+    }, [])
+
+    const checkDriving = React.useCallback(({ target: { checked } }) => {
+        checked && setTravelMode('DRIVING')
+    }, [])
+
+    const checkBicycling = React.useCallback(({ target: { checked } }) => {
+        checked && setTravelMode('BICYCLING')
+    }, [])
+
+    const checkTransit = React.useCallback(({ target: { checked } }) => {
+        checked && setTravelMode('TRANSIT')
+    }, [])
+
+    const checkWalking = React.useCallback(({ target: { checked } }) => {
+        checked && setTravelMode('WALKING')
+    }, [])
+
+    const onClick = React.useCallback(() => {
+        if (originRef.current.value !== '' && destinationRef.current.value !== '') {
+            setOrigin(originRef.current.value)
+            setDestination(destinationRef.current.value)
+            console.log('click on Build Route 01')
+        }
+        console.log('click on Build Route 02')
+    }, [])
+
+    const onMapClick = React.useCallback((...args) => {
+        console.log('onClick args: ', args)
+    }, [])
+
+    const directionsServiceOptions = React.useMemo(() => {
+        return {
+            destination: destination,
+            origin: origin,
+            travelMode: travelMode,
+        }
+    }, [])
+
+    const directionsRendererOptions = React.useMemo(() => {
+        return {
+            directions: response,
+        }
+    }, [])
+
     if (loadError) return "Error";
     if (!isLoaded) return "Loading...";
 
     return (
         <div>
+            <div className='map-settings'>
+                <hr className='mt-0 mb-3' />
+
+                <div className='row'>
+                    <div className='col-md-6 col-lg-4'>
+                        <div className='form-group'>
+                            <label htmlFor='ORIGIN'>Origin</label>
+                            <br />
+                            <input
+                                id='ORIGIN'
+                                className='form-control'
+                                type='text'
+                                ref={originRef}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='col-md-6 col-lg-4'>
+                        <div className='form-group'>
+                            <label htmlFor='DESTINATION'>Destination</label>
+                            <br />
+                            <input
+                                id='DESTINATION'
+                                className='form-control'
+                                type='text'
+                                ref={destinationRef}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='d-flex flex-wrap'>
+                    <div className='form-group custom-control custom-radio mr-4'>
+                        <input
+                            id='DRIVING'
+                            className='custom-control-input'
+                            name='travelMode'
+                            type='radio'
+                            checked={travelMode === 'DRIVING'}
+                            onChange={checkDriving}
+                        />
+                        <label className='custom-control-label' htmlFor='DRIVING'>
+                            Driving
+                        </label>
+                    </div>
+
+                    <div className='form-group custom-control custom-radio mr-4'>
+                        <input
+                            id='BICYCLING'
+                            className='custom-control-input'
+                            name='travelMode'
+                            type='radio'
+                            checked={travelMode === 'BICYCLING'}
+                            onChange={checkBicycling}
+                        />
+                        <label className='custom-control-label' htmlFor='BICYCLING'>
+                            Bicycling
+                        </label>
+                    </div>
+
+                    <div className='form-group custom-control custom-radio mr-4'>
+                        <input
+                            id='TRANSIT'
+                            className='custom-control-input'
+                            name='travelMode'
+                            type='radio'
+                            checked={travelMode === 'TRANSIT'}
+                            onChange={checkTransit}
+                        />
+                        <label className='custom-control-label' htmlFor='TRANSIT'>
+                            Transit
+                        </label>
+                    </div>
+
+                    <div className='form-group custom-control custom-radio mr-4'>
+                        <input
+                            id='WALKING'
+                            className='custom-control-input'
+                            name='travelMode'
+                            type='radio'
+                            checked={travelMode === 'WALKING'}
+                            onChange={checkWalking}
+                        />
+                        <label className='custom-control-label' htmlFor='WALKING'>
+                            Walking
+                        </label>
+                    </div>
+                </div>
+
+                <button className='btn btn-primary' type='button' onClick={onClick}>
+                    Build Route
+                </button>
+
+            </div>
+
             <GoogleMap
                 // id="map"
                 mapContainerStyle={mapContainerStyle}
                 zoom={10}
                 center={center}
                 options={options}
-                // onClick={onMapClick}
-                // onLoad={onMapLoad}
+            // onClick={onMapClick}
+            // onLoad={onMapLoad}
             >
                 {POIData.features.map(POI => (
                     <Marker
-                        key = {POI.properties.poiid}
-                        position = {{
+                        key={POI.properties.poiid}
+                        position={{
                             lat: POI.geometry.coordinates[1],
                             lng: POI.geometry.coordinates[0]
                         }}
-                        onClick = {() => {
+                        onClick={() => {
                             setSelectedPOI(POI);
                         }}
-                        // icon = {{
-                        //     url: " ",
-                        //     scaledSize: new window.google.maps.Size(25, 25)
-                        // }}
+                    // icon = {{
+                    //     url: " ",
+                    //     scaledSize: new window.google.maps.Size(25, 25)
+                    // }}
                     />
                 ))}
 
                 {selectedPOI && (
                     <InfoWindow
-                        position = {{
+                        position={{
                             lat: selectedPOI.geometry.coordinates[1],
                             lng: selectedPOI.geometry.coordinates[0]
                         }}
-                        onCloseClick = {() => {
+                        onCloseClick={() => {
                             setSelectedPOI(null);
                         }}
                     >
                         <div>
                             <div >
-                                <img 
+                                <img
                                     src={selectedPOI.properties.imageUrl}
-                                    style = {imageStyle}
+                                    style={imageStyle}
                                 />
                             </div>
                             <div >
                                 <b>{selectedPOI.properties.NAME}</b>
-                                <br/>
+                                <br />
                                 Address: {selectedPOI.properties.ADDRESS}
-                                <br/>
+                                <br />
                                 Description: {selectedPOI.properties.description}
                             </div>
                         </div>
                     </InfoWindow>
                 )}
+
+                {destination !== '' && origin !== '' && (
+                    <DirectionsService
+                        options={directionsServiceOptions}
+                        callback={directionsCallback}
+                    />
+                )}
+
+                {response !== null && (
+                    <DirectionsRenderer options={directionsRendererOptions} />
+                )}
+
             </GoogleMap>
         </div>
     );
 }
 
+Map.propTypes = DirectionsPropTypes
 export default Map
