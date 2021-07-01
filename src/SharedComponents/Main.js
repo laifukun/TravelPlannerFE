fa606141eb562255b1479d30ed520e267267c6e6
Chong Xu <cx44@duke.edu>
2021-07-01 23:18:27 +0800

    footer

--- src/SharedComponents/Main.js
+++ src/SharedComponents/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Layout, Button, message} from "antd";
+import {Layout, Button, message, Modal} from "antd";
 import POIInstruction from  './POIInstruction';
 import LoginForm from "../BeforeLogin/LoginForm";
 import RegisterForm from "../BeforeLogin/RegisterForm";
@@ -21,6 +21,9 @@ function Main(props) {
     const [pickedPOI, setPickedPOI] = useState();
     const [user, setUser] =useState();
     const [route, setRoute] = useState();
+    const [isStatVisible, setIsStatVisible] = useState(false);
+    const [isStaffVisible, setIsStaffVisible] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
 
     const initCenter = ({
@@ -51,6 +54,36 @@ function Main(props) {
         setAuthed(false);
     }
 
+    function showModal(input) {
+        if (input === "about"){
+            setIsModalVisible(true);
+        } else if (input === "stat") {
+            setIsStatVisible(true);
+        } else {
+            setIsStaffVisible(true);
+        }
+    }
+    
+    
+    const handleOk = (input) => {
+        if (input === "about"){
+            setIsModalVisible(false);
+        } else if (input === "stat") {
+            setIsStatVisible(false);
+        } else {
+            setIsStaffVisible(false);
+        }
+      };
+    
+    const handleCancel = (input) => {
+        if (input === "about"){
+            setIsModalVisible(false);
+        } else if (input === "stat") {
+            setIsStatVisible(false);
+        } else {
+            setIsStaffVisible(false);
+        }
+    };
 
     return (
         <Layout style={{display: "flex", justifyContent: "space-between", height: "100vh" }}>
@@ -93,8 +126,33 @@ function Main(props) {
             </div>
         </ Content>
 
-        <Footer className="footer">
+        <Footer className="footer" style = {{paddingLeft: 0, paddingRight: 0}}>
                 ©2021 Travel Planner. All Rights Reserved. Developed by FLAG Team 2
+                <div style = {{backgroundColor: "#282c34", width: "100vw" }}>
+                    <div style = {{display: "inline-block", width: "50%", textAlign: "center"}}> Contact Us: laiTeam2@gmail.com </div>
+                    
+                    <Button type="primary" onClick={() => showModal("stat")} style = {{width: "50%", backgroundColor: "#282c34", border: "none"}}>Privacy Statement</Button>
+                    <Modal visible={isStatVisible} onOk={() => handleOk("stat")} onCancel={() => handleCancel("stat")} style = {{width: "50%"}}>
+                    This Privacy Policy explains how TravelPlanner ( “we”or “us”) collects, uses, and discloses information about you. This Privacy Policy applies when you use our websites, mobile applications, and other online products and services that link to this Privacy Policy (collectively, our “Services”), contact our customer service team, engage with us on social media, or otherwise interact with us.
+                    We may change this Privacy Policy from time to time. If we make changes, we will notify you by revising the date at the top of this policy and, in some cases, we may provide you with additional notice (such as adding a statement to our website or providing you with a notification). We encourage you to review this Privacy Policy regularly to stay informed about our information practices and the choices available to you.
+                    </Modal>
+                
+                    <Button type="primary" onClick={() => showModal("staff")} style = {{width: "50%", backgroundColor: "#282c34", border: "none"}}>Staff</Button>
+                    <Modal visible={isStaffVisible} onOk={() => handleOk("staff")} onCancel={() => handleCancel("staff")} style = {{width: "50%"}}>
+                    Fukun Lai;
+                    Zheng Xie;
+                    Jian Song;
+                    Chong Xu;
+                    Kun Yu;
+                    Yuyang Li;
+                    Zhaoru Shang;
+                    </Modal>
+                    
+                    <Button type="primary" onClick={() => showModal("about")} style = {{width: "50%", backgroundColor: "#282c34", border: "none"}}>About Us</Button>
+                    <Modal visible={isModalVisible} onOk={() => handleOk("about")} onCancel={() => handleCancel("about")} style = {{width: "50%"}}>
+                    Most often, people have some rough ideas of the locations/attractions they want to visit, but they normally have no clear idea how to plan their trip to best suit their schedule and interest. The intent of a travel planner is to provide a tool to fill this gap. Users are able to arrange and plan their trip with travel planner according to their schedule and interest.
+                    </Modal>
+                </div>
         </Footer>
     </Layout>
     );
